30c6124eb60ed445e806d5954cb2763a6280ee15
rtmiranda <rtmiranda008@gmail.com>
2025-05-21 20:06:06 -0300

    ''

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { HistoricoComponent } from './pages/historico/historico/historico.component';
 import { AdotanteListComponent } from './pages/adotantes/adotante-list/adotante-list.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PetListComponent } from './pages/pets/pet-list/pet-list.component';
@@ -9,7 +8,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'pets', component: PetListComponent, canActivate: [AuthGuard] },
   { path: 'adotantes', component: AdotanteListComponent, canActivate: [AuthGuard] },
-  { path: 'historico', component: HistoricoComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'pets', pathMatch: 'full' },
   { path: '**', redirectTo: 'pets' }
 ];
